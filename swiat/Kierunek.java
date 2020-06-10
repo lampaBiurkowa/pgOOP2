f303ceb4b374711bbd1ce64f9160a0e4cb1fa5d1
Darek <mrocznaklawiatura@gmail.com>
2020-06-10 14:00:24 +0200

    :D

---
+++ swiat/Kierunek.java
@@ -0,0 +1,3 @@
+package pl.edu.pg.eti.ksg.po.projekt2.swiat;
+
+public enum Kierunek {LEWO, PRAWO, DOL, GORA, BRAK}
---
+++ swiat/ObslugaRuchu.java
@@ -0,0 +1,117 @@
+package pl.edu.pg.eti.ksg.po.projekt2.swiat;
+
+import pl.edu.pg.eti.ksg.po.projekt2.organizmy.zwierzeta.Czlowiek;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public final class ObslugaRuchu
+{
+    private static final int BOK_PRZYCISKU = 60;
+    private static final int X_OKNA = 800;
+    private static final int Y_OKNA = 100;
+
+    private JFrame okno;
+    private JButton lewo;
+    private JButton prawo;
+    private JButton gora;
+    private JButton dol;
+    private Kierunek wybor;
+
+    public ObslugaRuchu(Swiat swiat, Czlowiek czlowiek)
+    {
+        wybor = Kierunek.BRAK;
+        okno = new JFrame("Wybierz kieruek");
+        okno.setLayout(null);
+        okno.setVisible(true);
+        okno.setBounds(X_OKNA, Y_OKNA, 3 * BOK_PRZYCISKU, 3 * BOK_PRZYCISKU);
+        dodajStrzalki(swiat, czlowiek);
+    }
+
+    private void dodajStrzalki(Swiat swiat, Czlowiek czlowiek)
+    {
+        dodajStrzalkeWLewo(swiat, czlowiek);
+        dodajStrzalkeWPrawo(swiat, czlowiek);
+        dodajStrzalkeWGore(swiat, czlowiek);
+        dodajStrzalkeWDol(swiat, czlowiek);
+    }
+
+    private void dodajStrzalkeWLewo(Swiat swiat, Czlowiek czlowiek)
+    {
+        lewo = new JButton("<-");
+        if (czlowiek.GetX() == 0)
+            lewo.setEnabled(false);
+        lewo.setBounds(0, BOK_PRZYCISKU, BOK_PRZYCISKU, BOK_PRZYCISKU);
+        lewo.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                wybor = Kierunek.LEWO;
+            }
+        });
+        okno.add(lewo);
+    }
+
+    private void dodajStrzalkeWPrawo(Swiat swiat, Czlowiek czlowiek)
+    {
+        prawo = new JButton("->");
+        if (czlowiek.GetX() == swiat.GetSzerokosc() - 1)
+            prawo.setEnabled(false);
+        prawo.setBounds(2 * BOK_PRZYCISKU, BOK_PRZYCISKU, BOK_PRZYCISKU, BOK_PRZYCISKU);
+        prawo.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                wybor = Kierunek.PRAWO;
+            }
+        });
+        okno.add(prawo);
+    }
+
+    private void dodajStrzalkeWGore(Swiat swiat, Czlowiek czlowiek)
+    {
+        gora = new JButton("/\\");
+        if (czlowiek.GetY() == 0)
+            gora.setEnabled(false);
+        gora.setBounds(BOK_PRZYCISKU, BOK_PRZYCISKU, BOK_PRZYCISKU, BOK_PRZYCISKU);
+        gora.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                wybor = Kierunek.GORA;
+            }
+        });
+        okno.add(gora);
+    }
+
+    private void dodajStrzalkeWDol(Swiat swiat, Czlowiek czlowiek)
+    {
+        dol = new JButton("\\/");
+        if (czlowiek.GetY() == swiat.GetWysokosc() - 1)
+            dol.setEnabled(false);
+        dol.setBounds(BOK_PRZYCISKU, 0, BOK_PRZYCISKU, BOK_PRZYCISKU);
+        dol.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                wybor = Kierunek.DOL;
+            }
+        });
+        okno.add(dol);
+    }
+
+    public Kierunek CzekajNaWybor()
+    {
+        while (true)
+        {
+            try
+            {
+                wait(100);
+            } catch (Exception e) { }
+
+            if (wybor != Kierunek.BRAK)
+                return wybor;
+        }
+    }
+}
